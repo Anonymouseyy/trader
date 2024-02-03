0def8a997cbdcc7556a179b467a73453cf50635f
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-03 12:43:33 -0800

    trying and failing to get format

---
+++ client/pages/(primary)/page.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+
+export default function Home() {
+	return (
+		<header className="">
+    	  <h1 className='flex flex-col text-6xl gap-6 font-bold w-full items-center text-center='>
+        	Trader
+    	  </h1>
+  		</header>
+	  )
+}
--- client/pages/layout.tsx
+++ client/pages/layout.tsx
@@ -1,7 +1,7 @@
-import { Lato } from 'next/font/google'
-import './globals.css'
+import { Sansita_Swashed } from 'next/font/google'
+import './globals.css';
 
-const lato = Lato({ subsets: ['latin'], weight: ['100', '300', '400', '700', '900'] })
+const sansita = Sansita_Swashed({ subsets: ['latin'], weight: ['300', '400', '900'] })
 
 export default function RootLayout({
   children,
@@ -9,14 +9,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="bg-[#1e2630] selection:bg-[#b4d5fe66]">
-      <head>
-        <meta name="color-scheme" content="dark" />
-        {/* eslint-disable @next/next/no-css-tags */}
-        <noscript><link rel="stylesheet" href="/noscript.css" /></noscript>
-        {/* eslint-enable @next/next/no-css-tags */}
-      </head>
-      <body className={lato.className + ' text-white'}>{children}</body>
+    <html lang="en" className="bg-[#262626]">
+      <body className={sansita.className + ' text-white'}>{children}</body>
     </html>
   )
 }
