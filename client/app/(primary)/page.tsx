7edef99bb5ea59dd171d89746f71b9d67d6ec628
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-05 07:12:23 -0800

    finish design and start layout

--- client/app/(primary)/page.tsx
+++ client/app/(primary)/page.tsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from "react"
 
 export default function Home() {
 	return (
-		<header>
-    	  <h1 className='flex flex-col text-6xl gap-6 font-bold w-full items-center text-center='>
-        	Trader
-    	  </h1>
-  		</header>
+		<></>
 	  )
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Sansita_Swashed } from 'next/font/google'
+import Image from 'next/image'
 import './globals.css';
 
 const sansita = Sansita_Swashed({ subsets: ['latin'], weight: ['300', '400', '900'] })
@@ -10,7 +11,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-[#262626]">
-      <body className={sansita.className + ' text-white'}>{children}</body>
+      <body className={sansita.className + ' text-white px-[2.5%] py-[32px]'}>
+        <header className="flex-row w-full">
+    	    <h1 className='text-6xl gap-6 font-bold w-[50%] items-left text-left'>
+        	  Trader
+    	    </h1>
+          <div className='flex-row text-6xl gap-6 font-bold w-[50%] items-left text-left justify-end'>
+            <h1 className='text-5xl gap-6 font-bold items-left text-left'>
+        	    History
+    	      </h1>
+            <Image src="/blankpfp.png" alt="blank" width="64" height="64"></Image>
+          </div>
+  		  </header>
+        {children}
+      </body>
     </html>
   )
 }
