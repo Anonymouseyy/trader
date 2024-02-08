45927f855be42f4eb8e238b17fac22294e980f94
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-07 21:06:13 -0800

    yes good

--- client/app/(primary)/page.tsx
+++ client/app/(primary)/page.tsx
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from "react"
 
 export default function Home() {
 	return (
-		<>
-		  <div className="flex flex-col w-[50%] h-[100%]">
-			<div className="w-full h-[48%] py-[1%] bg-[#333333]">
+		<div className="flex flex-row w-full h-[94%] px-3 py-5">
+		  <div className="flex flex-col w-[50%] h-full pr-2">
+			<div className="w-full h-[28%] my-[1%] bg-[#333333] rounded-2xl">
+
+			</div>
+			<div className="w-full h-[68%] my-[1%] bg-[#333333] rounded-2xl">
 
 			</div>
 		  </div>
-		  <div>
-			
+		  <div className="flex flex-col w-[50%] h-full pl-2">
+		    <div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl">
+
+		    </div>
+  		    <div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl">
+
+			</div>
 		  </div>
-		</>
+		</div>
 	  )
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-[#262626]">
       <body className={sansita.className + ' text-white px-[2.5%] py-[32px] h-[100vh]'}>
-        <header className="flex flex-row w-full">
+        <header className="flex flex-row w-full pb-3">
     	    <h1 className='flex text-6xl gap-6 font-bold text-left w-[50%]'>
         	  Trader
     	    </h1>
