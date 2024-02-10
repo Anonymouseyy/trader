71b69fb5f20c9744cc418a5fba11b69efffb9d4b
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-09 23:31:16 -0800

    start clock

--- client/app/(primary)/page.tsx
+++ client/app/(primary)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<div className="flex flex-row w-full h-[94%] px-3 py-5">
 		  <div className="flex flex-col w-[50%] h-full pr-2">
 			<div className="w-full h-[28%] my-[1%] bg-[#333333] rounded-2xl">
-
+				
 			</div>
 			<div className="w-full h-[68%] my-[1%] bg-[#333333] rounded-2xl">
 
