86c940d345f5e9a593c64834e6cd44bb27006526
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-12 21:41:14 -0800

    add market, styling

--- client/app/(primary)/page.tsx
+++ client/app/(primary)/page.tsx
@@ -11,19 +11,27 @@ export default function Home() {
 		day: 'numeric',
 	};
 
+	let market = "closed"
+	if ((d.getUTCHours() < 23 && d.getUTCHours() > 14) || (d.getUTCHours() >= 13 && d.getUTCMinutes() >= 30)) {
+		market = "open"
+	}
+
 	return (
 		<div className="flex flex-row w-full h-[94%] px-3 py-5">
 		  <div className="flex flex-col w-[50%] h-full pr-2">
 			<div className="w-full h-[28%] my-[1%] bg-[#333333] rounded-2xl">
-				<div className="flex flex-row w-full m-4">
-					<h2 className="text-5xl">
+				<div className="flex flex-row w-full py-6 px-7 place-content-between">
+					<h2 className="text-4xl">
 						{ d.toLocaleDateString(undefined, options) }
 					</h2>
 					<Clock 
-          				format={'h:mm'} 
+          				format={'h:mm:ss a'} 
           				ticking={true}
-						className="self-end text-5xl " /> 
+						className="text-4xl align-right"/> 
 				</div>
+				<h2 className="text-6xl w-full text-center m-2">
+					Market is { market }
+				</h2>
 			</div>
 			<div className="w-full h-[68%] my-[1%] bg-[#333333] rounded-2xl">
 
