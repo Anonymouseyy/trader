35ff2fac2869bc641345c3b0ddd549efa23fa517
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-27 22:40:03 -0800

    reformatting and form

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 
 export default function Account() {
+    async function updateInfo(event: React.FormEvent<HTMLFormElement>) {
+
+    }
+
     return (
         <>
-          <div className="w-[70%] h-[70%] mt-[5%] bg-[#333333] rounded-2xl mx-[15%] p-5">
-            <h1 className='text-6xl font-bold text-left'>
-                Account
-            </h1>
-            <div className="flex">
-              
-            </div>
-          </div>
+          	<div className="w-[70%] h-[70%] mt-[5%] bg-[#333333] rounded-2xl mx-[15%] p-5">
+            	<h1 className='text-6xl font-bold text-left'>
+                	Account
+           		</h1>
+            	<div className="flex">
+              		<form onSubmit={updateInfo}>
+
+              		</form>
+            	</div>
+          	</div>
         </>
     )
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -27,67 +27,67 @@ export default function Home() {
 
 	return (
 		<div className="flex flex-row w-full h-[94%] px-3 py-5">
-		  <div className="flex flex-col w-[50%] h-full pr-2">
-			<div className="w-full h-[28%] my-[1%] bg-[#333333] rounded-2xl">
-				<div className="flex flex-row w-full py-6 px-7 place-content-between">
-					<h2 className="text-4xl">
-						{ d.toLocaleDateString('en-US', { weekday: "long", year: "numeric", month: "long", day: "numeric", }) }
+		  	<div className="flex flex-col w-[50%] h-full pr-2">
+				<div className="w-full h-[28%] my-[1%] bg-[#333333] rounded-2xl">
+					<div className="flex flex-row w-full py-6 px-7 place-content-between">
+						<h2 className="text-4xl">
+							{ d.toLocaleDateString('en-US', { weekday: "long", year: "numeric", month: "long", day: "numeric", }) }
+						</h2>
+						<Clock 
+          					format={'h:mm:ss a'} 
+          					ticking={true}
+							className="text-4xl align-right"/> 
+					</div>
+					<h2 className="text-6xl w-full text-center m-2">
+						Market is { market }
 					</h2>
-					<Clock 
-          				format={'h:mm:ss a'} 
-          				ticking={true}
-						className="text-4xl align-right"/> 
 				</div>
-				<h2 className="text-6xl w-full text-center m-2">
-					Market is { market }
-				</h2>
-			</div>
-			<div className="w-full h-[68%] my-[1%] bg-[#333333] rounded-2xl">
+				<div className="w-full h-[68%] my-[1%] bg-[#333333] rounded-2xl">
 				
-			</div>
-		  </div>
-		  <div className="flex flex-col w-[50%] h-full pl-2">
-		    <div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl py-6 px-7">
-				<h2 className="text-4xl mb-2"> 
-					Leaderboard
-				</h2>
-				<div className="flex flex-row w-full bg-[#232323] h-[15%] rounded-xl my-2 items-center">
-					<p className="text-xl w-[20%] text-center">Rank</p>
-					<p className="text-xl w-[30%] text-center">Name</p>
-					<p className="text-xl w-[25%] text-center">Net Worth</p>
-					<p className="text-xl w-[25%] text-center">Trades</p>
-				</div>
-				<div className="overflow-y-scroll h-[73%]">
-					{ lb.map((entry) => (
-						<div className="flex flex-row w-full bg-[#434343] h-12 rounded-xl my-2 items-center" key={ entry.rank }>
-							<p className="text-xl w-[20%] text-center">{ entry.rank }</p>
-							<p className="text-xl w-[30%] text-center">{ entry.player }</p>
-							<p className="text-xl w-[25%] text-center">{ entry.portfolio_value }</p>
-							<p className="text-xl w-[25%] text-center">{ entry.transactions }</p>
-						</div>
-					))} 
-				</div>
-		    </div>
-  		    <div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl py-6 px-7">
-			  <h2 className="text-4xl mb-2"> 
-					Watchlist
-				</h2>
-				<div className="flex flex-row w-full bg-[#232323] h-[15%] rounded-xl my-2 items-center">
-					<p className="text-xl w-[20%] text-center">Ticker</p>
-					<p className="text-xl w-[60%] text-center">Company</p>
-					<p className="text-xl w-[20%] text-center">Price</p>
-				</div>
-				<div className="overflow-y-scroll h-[73%]">
-					{ wl.map((entry) => (
-						<div className="flex flex-row w-full bg-[#434343] h-12 rounded-xl my-2 items-center" key={ entry.ticker }>
-							<p className="text-xl w-[20%] text-center">{ entry.ticker }</p>
-							<p className="text-xl w-[60%] text-center">{ entry.company }</p>
-							<p className="text-xl w-[20%] text-center">{ entry.price }</p>
-						</div>
-					))} 
 				</div>
 			</div>
-		  </div>
+		  	<div className="flex flex-col w-[50%] h-full pl-2">
+		    	<div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl py-6 px-7">
+					<h2 className="text-4xl mb-2"> 
+						Leaderboard
+					</h2>
+					<div className="flex flex-row w-full bg-[#232323] h-[15%] rounded-xl my-2 items-center">
+						<p className="text-xl w-[20%] text-center">Rank</p>
+						<p className="text-xl w-[30%] text-center">Name</p>
+						<p className="text-xl w-[25%] text-center">Net Worth</p>
+						<p className="text-xl w-[25%] text-center">Trades</p>
+					</div>
+					<div className="overflow-y-scroll h-[73%]">
+						{ lb.map((entry) => (
+							<div className="flex flex-row w-full bg-[#434343] h-12 rounded-xl my-2 items-center" key={ entry.rank }>
+								<p className="text-xl w-[20%] text-center">{ entry.rank }</p>
+								<p className="text-xl w-[30%] text-center">{ entry.player }</p>
+								<p className="text-xl w-[25%] text-center">{ entry.portfolio_value }</p>
+								<p className="text-xl w-[25%] text-center">{ entry.transactions }</p>
+							</div>
+						))} 
+					</div>
+		    	</div>
+  		    	<div className="w-full h-[48%] my-[1%] bg-[#333333] rounded-2xl py-6 px-7">
+			  		<h2 className="text-4xl mb-2"> 
+						Watchlist
+					</h2>
+					<div className="flex flex-row w-full bg-[#232323] h-[15%] rounded-xl my-2 items-center">
+						<p className="text-xl w-[20%] text-center">Ticker</p>
+						<p className="text-xl w-[60%] text-center">Company</p>
+						<p className="text-xl w-[20%] text-center">Price</p>
+					</div>
+					<div className="overflow-y-scroll h-[73%]">
+						{ wl.map((entry) => (
+							<div className="flex flex-row w-full bg-[#434343] h-12 rounded-xl my-2 items-center" key={ entry.ticker }>
+								<p className="text-xl w-[20%] text-center">{ entry.ticker }</p>
+								<p className="text-xl w-[60%] text-center">{ entry.company }</p>
+								<p className="text-xl w-[20%] text-center">{ entry.price }</p>
+							</div>
+						))} 
+					</div>
+				</div>
+		  	</div>
 		</div>
 	  )
 }
