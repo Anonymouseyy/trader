599d1ad817506064fed2bbcec1739c87558ce259
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-28 20:23:46 -0800

    account settings page

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -7,15 +7,24 @@ export default function Account() {
 
     return (
         <>
-          	<div className="w-[70%] h-[70%] mt-[5%] bg-[#333333] rounded-2xl mx-[15%] p-5">
-            	<h1 className='text-6xl font-bold text-left'>
+          	<div className="w-[35%] h-[auto] mt-[5%] bg-[#333333] rounded-2xl m-[auto] p-5">
+            	<h1 className='text-6xl font-bold text-center'>
                 	Account
            		</h1>
-            	<div className="flex">
-              		<form onSubmit={updateInfo}>
-
-              		</form>
-            	</div>
+              	<form className="flex w-[100%] py-5 flex-col items-center" onSubmit={updateInfo}>
+					<div className="flex w-[100%] items-center p-3">
+						<label htmlFor="mail" className="text-3xl mx-3 w-[30%]">Email: </label>
+						<input type="email" id="mail" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2"></input>
+					</div>
+					<div className="flex w-[100%] items-center p-3">
+						<label htmlFor="password" className="text-3xl mx-3 w-[30%]">Password: </label>
+						<input type="password" id="password" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2"></input>
+					</div>
+					<div className="flex w-[100%] items-center p-3">
+						<label htmlFor="watchlist" className="text-3xl mx-3 w-[30%]">Watchlist: </label>
+						<input type="text" id="watchlist" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2"></input>
+					</div>
+              	</form>
           	</div>
         </>
     )
