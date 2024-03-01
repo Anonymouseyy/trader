b3ba8dcc894e1c6255c06b0400332d8f2339c5ee
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-02-29 20:12:12 -0800

    submit button

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -7,11 +7,11 @@ export default function Account() {
 
     return (
         <>
-          	<div className="w-[35%] h-[auto] mt-[5%] bg-[#333333] rounded-2xl m-[auto] p-5">
+          	<div className="w-[35%] h-[auto] mt-[5%] bg-[#333333] rounded-2xl m-[auto] pt-5 px-5">
             	<h1 className='text-6xl font-bold text-center'>
                 	Account
            		</h1>
-              	<form className="flex w-[100%] py-5 flex-col items-center" onSubmit={updateInfo}>
+              	<form className="flex w-[100%] py-5 flex-col items-center" onSubmit={updateInfo} method="post">
 					<div className="flex w-[100%] items-center p-3">
 						<label htmlFor="mail" className="text-3xl mx-3 w-[30%]">Email: </label>
 						<input type="email" id="mail" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2"></input>
@@ -24,6 +24,7 @@ export default function Account() {
 						<label htmlFor="watchlist" className="text-3xl mx-3 w-[30%]">Watchlist: </label>
 						<input type="text" id="watchlist" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2"></input>
 					</div>
+					<button type="submit" className="text-2xl w-[30%] h-10 bg-[#0085FF] rounded-xl m-2">Save</button>
               	</form>
           	</div>
         </>
