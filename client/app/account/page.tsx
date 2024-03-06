f0a033ba1e7c3887272c9081182a37d592888251
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-03-05 19:06:52 -0800

    fetch info on clientside

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -4,7 +4,13 @@ import React, { useEffect, useState } from "react"
 export default function Account() {
 	const [message, setMessage] = useState("")
 	const [isLoading, setIsLoading] = useState(false)
-	const [user, setUser] = useState({ email: "", password: "", watchlist: "" });
+	const [user, setUser] = useState({ email: "...", password: "...", watchlist: "..." });
+
+	useEffect(() => {
+		fetch("http://127.0.0.1:4201/api/account")
+		.then(response => response.json())
+		.then(data => setUser(data))
+	}, [])
 
     async function updateInfo(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault()
@@ -41,17 +47,17 @@ export default function Account() {
 					<div className="flex w-[100%] items-center p-3">
 						<label htmlFor="mail" className="text-3xl mx-3 w-[30%]">Email: </label>
 						<input type="email" id="mail" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2" 
-						onChange={(e) => setUser({ ...user, email: e.target.value })} required/>
+						onChange={(e) => setUser({ ...user, email: e.target.value })} value={user.email} required/>
 					</div>
 					<div className="flex w-[100%] items-center p-3">
 						<label htmlFor="password" className="text-3xl mx-3 w-[30%]">Password: </label>
 						<input type="password" id="password" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2" 
-						onChange={(e) => setUser({ ...user, password: e.target.value })} required/>
+						onChange={(e) => setUser({ ...user, password: e.target.value })} value={user.password} required/>
 					</div>
 					<div className="flex w-[100%] items-center p-3">
 						<label htmlFor="watchlist" className="text-3xl mx-3 w-[30%]">Watchlist: </label>
 						<input type="text" id="watchlist" width="" className="bg-[#232323] outline-[#434343] flex-1 h-10 rounded-xl px-2" 
-						onChange={(e) => setUser({ ...user, watchlist: e.target.value })} required/>
+						onChange={(e) => setUser({ ...user, watchlist: e.target.value })} value={user.watchlist} required/>
 					</div>
 					<button type="submit" className="text-2xl w-[30%] h-10 bg-[#0085FF] rounded-xl m-2" disabled={ isLoading }>{message ? message : "Save"}</button>
               	</form>
